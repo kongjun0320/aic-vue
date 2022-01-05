969a7a6d0633b7f7fe730bf223f960bd966fc3b2
kongjun0320 <1741142595@qq.com>
2022-01-05 16:36:36 +0800

    init & 搭建rollup环境

---
+++ NOTE.md
@@ -0,0 +1,7 @@
+# 依赖的作用
++ rollup 打包工具
++ @babel/core babel的核心模块
++ @babel/preset-env 将高级语法转换成低级语法
++ rollup-plugin-babel 在 rollup 中使用 babel
++ rollup-plugin-serve 实现静态服务
++ cross-env 设置环境变量
---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import babel from 'rollup-plugin-babel'
+import serve from 'rollup-plugin-serve'
+
+export default {
+  input: './src/index.js',
+  output: {
+    file: 'dist/umd/vue.js',
+    name: 'Vue', // 使用 umd 规范在浏览器中使用 全局变量的名称
+    format: 'umd', // 打包的模块化规范
+    sourcemap: true // 源代码和运行代码的映射
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    process.env.ENV === 'development'
+      ? serve({
+          open: true,
+          openPage: '/public/index.html',
+          port: 3300,
+          contentBase: ''
+        })
+      : null
+  ]
+}
